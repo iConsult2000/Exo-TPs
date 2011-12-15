ee09c0b406c835444b0c6bffdbbdb668235185e4
boston <yohann.bray@gmail.com>
2011-12-15 18:00:12 +0100

    my tp shop

---
+++ YBY_JspFirstServlet/WebContent/accueil.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>First Servlet</title>
+<style type="text/css">
+  #menu li {
+  color : red;
+  float: left;
+  font-size:14px;
+  list-style-type:none;
+  margin-left:10px;
+ 
+ }
+  </style>
+</head>
+<body>
+	<%@ include file="menu.jsp"%>
+	<br>
+	<hr>
+	<div>
+		<p>Bienvenue sur site généralissime <br> Pour passez à la page secret vous devez vous connecter</p> 
+	</div>
+</body>
+</html>
---
+++ YBY_JspFirstServlet/src/com/insia/org/servlets/ProduitServlet.java
@@ -0,0 +1,51 @@
+package com.insia.org.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.insia.org.beans.Produit;
+
+/**
+ * Servlet implementation class produitServlet
+ */
+@WebServlet("/produitServlet")
+public class ProduitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProduitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Produit montre = new Produit();
+		montre.setLibelle("Rollex x510");
+		montre.setPrix(469);
+		
+		request.getSession(true).setAttribute("article", montre);
+		
+		request.getRequestDispatcher("/produit.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
